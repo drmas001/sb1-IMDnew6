1b53da9116db8f9c5243ac32d5affa6909b820cf
drmas001 <git@stackblitz.com>
2024-11-08 15:01:41 +0300

    Initial commit

---
+++ add_admin_drmas.sql
@@ -0,0 +1,14 @@
+-- Insert admin user with medical code M1019
+INSERT INTO users (
+    medical_code,
+    name,
+    role,
+    department,
+    status
+) VALUES (
+    'M1019',
+    'Dr.Mohammed Aedh Alshehri',
+    'administrator',
+    'Administration',
+    'active'
+);
